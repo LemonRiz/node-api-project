b2bbe0decc9d9eab84753a9f93b8b2a2d8bc456f
LemonRiz <95.m.rizvi@gmail.com>
2023-03-01 13:35:41 +0000

    added stcript

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("todolist_db", "root", "password", {
-  host: "localhost",
+export const sequelize = new Sequelize("todolist_db", $DB_USER, $DB_PW, {
+  host: $DB_HOST,
   dialect: "mysql",
 });
